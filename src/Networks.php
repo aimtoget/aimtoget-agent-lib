9fa15a2f018a16e504347fe1868d5a0dc3ffc249
Salawu Oluwadamilola Ezekiel <damilolaoffical@gmail.com>
2020-04-25 12:58:14 +0100

    added phone number validation

--- src/Networks.php
+++ src/Networks.php
@@ -2,6 +2,9 @@
 
 namespace Aimtoget\Agent;
 
+use Aimtoget\Agent\Exceptions\ServiceException;
+use GuzzleHttp\RequestOptions;
+
 class Networks extends Main
 {
     /**
@@ -16,5 +19,23 @@ class Networks extends Main
         $data = json_decode($content);
 
         return $data->data->networks;
+    }
+
+    public function validatePhone(string $phone)
+    {
+        $res = $this->_config->getClient()->request('POST', 'phone/validate', [
+            RequestOptions::JSON => array(
+                'phone' => $phone
+            )
+        ]);
+
+        $response = $res->getBody()->getContents();
+        $data = json_decode($response);
+
+        if($data->status === Config::STATUS_ERROR) {
+            throw new ServiceException($res->msg);
+        }
+
+        return $data->data->validated;
     }
 }
